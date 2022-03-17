a7efa0a3d59615ae09f0f5e1314066909bc92fd2
Blair Preston <blair.preston@student.awesomeinc.org>
2022-03-17 17:02:43 +0000

    update above the fold

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../App.css';
 import background from '../wireframes/background.png';
+import openpic from '../wireframes/taco.png';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
@@ -9,10 +11,12 @@ function Home() {
             <div className="home-left">
               <h1 style={{ color:"green", fontSize:"7em", fontWeight:"bold", marginBottom:"0" }}>Restaurant</h1> 
               <p style={{ color:"#624a3c", fontSize:"3em", marginTop:"0" }}>type of food/ slogan text</p>
-            <button style={{ color:"green", backgroundColor:"transparent", borderRadius:"25px", border:"solid 2px green", height:"4rem", width:"8rem", fontSize:"2rem" }}>Menu</button>
+            <button style={{ color:"green", backgroundColor:"transparent", borderRadius:"25px", border:"solid 2px green", height:"4rem", width:"8rem", fontSize:"2rem" }}>
+              <Link style={{ textDecoration:"none", color:"green" }} to="/menu">Menu</Link> 
+            </button>
           </div>
           <div className="home-right">
-            <h1>picture</h1>
+            <img src={openpic} ></img>
           </div>
         </div>
     </div>
